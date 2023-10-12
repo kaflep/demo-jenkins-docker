b7dd0c0ef8c57077ac9cfd8dff4f28ddfcdfa010
kafleP1994 <118851897+kafleP1994@users.noreply.github.com>
2023-10-11 23:16:07 -0500

    project create

---
+++ src/main/java/com/example/demo_for_springboot/DemoForSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo_for_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForSpringBootApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoForSpringBootApplication.class, args);
+	}
+
+}
